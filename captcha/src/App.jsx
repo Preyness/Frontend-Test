ba4e3d8cc0c427e5b666024f8d63eb3c065be3ba
Prince Kurt Laurence <delapenaprincekurtlaurence@gmail.com>
2024-07-23 05:09:22 +0800

    added a router page which is the app.jsx

--- captcha/src/App.jsx
+++ captcha/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Home from './LandingPage/Home';
+import Validation from './ValidationPage/Validation';
+import Result from './ResultPage/Result';
+/* This is where the routing takes place. I designed it this way to make routing between pages easier and cleaner. 
+Each page is imported here, allowing us to set up routes for them. */
+
+//react-router-dom is for routing functionalities
+// Validation, Home, Result are different pages to where the user is redirected
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/validation" element={<Validation />} />
+        <Route path="/result" element={<Result />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
